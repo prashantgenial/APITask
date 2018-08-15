d80640922225cb7908b02d90d14626598f094c18
Prashant G <prashant.gupta.ext@orange.com>
2018-08-15 22:00:12 +0530

    removed unwanted logging

--- src/main/java/com/prashant/examples/controller/MediaController.java
+++ src/main/java/com/prashant/examples/controller/MediaController.java
@@ -1,7 +1,5 @@
 package com.prashant.examples.controller;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
@@ -9,10 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -20,11 +15,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import com.prashant.examples.MediaDownloader;
-import com.prashant.examples.client.books.Book;
-import com.prashant.examples.client.itunes.Track;
 import com.prashant.examples.exception.APIExceptionDetaile;
 import com.prashant.examples.exception.APINotFoundException;
 import com.prashant.examples.model.Media;
@@ -32,12 +24,12 @@ import com.prashant.examples.model.Media;
 @RestController
 public class MediaController {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(MediaController.class);
+	//private static final Logger LOGGER = LoggerFactory.getLogger(MediaController.class);
 	
 	@Value("${books.record.count}")
 	private Integer records;
 	
-	@RequestMapping(method=RequestMethod.GET,value="/media1")
+	@RequestMapping(method=RequestMethod.GET,value="/media")
 	public List<Media> getMedia( @RequestParam(value="input") String input) throws APINotFoundException {
 		
 		if (StringUtils.isEmpty(input)) 
@@ -50,8 +42,6 @@ public class MediaController {
 		
 	@ExceptionHandler(MissingServletRequestParameterException.class)
 	public ResponseEntity<APIExceptionDetaile> handleMissingParams(MissingServletRequestParameterException ex) {
-	    String name = ex.getParameterName();
-	    System.out.println(name + " parameter is missing");
 	    APIExceptionDetaile errorDetails = new APIExceptionDetaile(new Date(), ex.getMessage(),
 	            ex.getParameterName() + " is required.");
 	        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);

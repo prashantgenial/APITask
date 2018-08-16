7ad2b3ff4f4c39751b28dd7910bb05d4f3071ab6
Prashant G <prashant.gupta.ext@orange.com>
2018-08-16 22:02:41 +0530

    refactored MediaDownloader

--- src/main/java/com/prashant/examples/MediaDownloader.java
+++ src/main/java/com/prashant/examples/MediaDownloader.java
@@ -2,8 +2,6 @@ package com.prashant.examples;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
@@ -12,6 +10,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
+import java.util.stream.Collectors;
 
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
@@ -29,39 +28,40 @@ import com.prashant.examples.model.Media;
 public class MediaDownloader {
 
 	private final ExecutorService executor = Executors.newFixedThreadPool(2);
-	
+		
 	private static final long TIME_FRAME = 2000000000L; // 2 seconds
 	
-	private static class BookDownloader implements Callable {
+	private static class Downloader implements Callable<List<Media>> {
 		private String input ;
 		private Integer records ;
-		public BookDownloader(String input,Integer records) {
+		private MyMediaType mediaType;
+		public Downloader(String input,Integer records,MyMediaType mediaType) {
 			this.input = input;
 			this.records = records ;
+			this.mediaType = mediaType ;
 		}
-		public Book call() {			
-			return getBook();
+		public List<Media> call() {
+			if(this.mediaType == MyMediaType.BOOK)
+				return getBook();
+			else
+				return getTracks();
 		}
 		
-		private Book getBook(){
+		private List<Media> getBook(){
+			List<Media> medias = new ArrayList<>();
 			RestTemplate restTemplate = new RestTemplate();
 			Book book = restTemplate.getForObject("https://www.googleapis.com/books/v1/volumes?q="+this.input+"&maxResults="+this.records, Book.class); 
-	        return book;
-		}
-	}
-	
-	private static class TrackDownloader implements Callable {
-		private String input ;
-		private Integer records ;
-		public TrackDownloader(String input,Integer records) {
-			this.input = input;
-			this.records = records ;
-		}
-		public Track call() {			
-			return getTracks();
+			List<Item> items = book.getItems();
+			for(Item item: items) {
+				//Media m1 = new Media(item.getVolumeInfo().getTitle(),item.getVolumeInfo().getAuthors().get(0),MyMediaType.BOOK);
+				if(!StringUtils.isEmpty(item.getVolumeInfo().getTitle()) && !StringUtils.isEmpty(item.getVolumeInfo().getAuthors()))
+					medias.add(new Media(item.getVolumeInfo().getTitle(),item.getVolumeInfo().getAuthors().get(0),MyMediaType.BOOK));
+			}
+	        return medias;
 		}
 		
-		private Track getTracks(){
+		private List<Media> getTracks(){
+			List<Media> medias = new ArrayList<>();
 			RestTemplate restTemplate = new RestTemplate();
 			String url = "https://itunes.apple.com/search?term="+this.input+"&limit="+this.records;
 			List<HttpMessageConverter<?>> messageConverters = new ArrayList<HttpMessageConverter<?>>();        
@@ -70,60 +70,39 @@ public class MediaDownloader {
 			messageConverters.add(converter);  
 			restTemplate.setMessageConverters(messageConverters);  
 			Track track = restTemplate.getForObject(url, Track.class);
-			return track;
+			List<Result> results = track.getResults();
+			for(Result result: results) {
+				//Media m2 = new Media(result.getTrackName(),result.getArtistName(),MyMediaType.SONG);
+				if(!StringUtils.isEmpty(result.getTrackName()) && !StringUtils.isEmpty(result.getArtistName()))
+					medias.add(new Media(result.getTrackName(),result.getArtistName(),MyMediaType.SONG));	
+			}
+			return medias;
 		}
 	}
-	
 
 	
 	public List<Media> go(String input,Integer records) {
 		List<Media> medias = new ArrayList<>();
-		Future<Book> bookFuture = executor.submit(new BookDownloader(input,records));
-		Future<Track> trackFuture = executor.submit(new TrackDownloader(input,records));
+		List<Future<List<Media>>> futures = new ArrayList<>();
+		futures.add(executor.submit(new Downloader(input,records,MyMediaType.BOOK)));
+		futures.add(executor.submit(new Downloader(input,records,MyMediaType.SONG)));
 		
-		try {
-			Book book = bookFuture.get(TIME_FRAME, TimeUnit.NANOSECONDS);
-			List<Item> items = book.getItems();
-			for(Item item: items) {
-				Media m1 = new Media(item.getVolumeInfo().getTitle(),item.getVolumeInfo().getAuthors().get(0),MyMediaType.BOOK);
-				if(!StringUtils.isEmpty(item.getVolumeInfo().getTitle()) && !StringUtils.isEmpty(item.getVolumeInfo().getAuthors()) && !medias.equals(m1))
-					medias.add(m1);
+		for (Future<List<Media>> future : futures) {
+			try {
+				List<Media> m = future.get(TIME_FRAME, TimeUnit.NANOSECONDS);
+				medias.addAll(m);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			} catch (ExecutionException e) {
+				e.printStackTrace();
+			} catch (TimeoutException e) {
+				future.cancel(true);
+				System.out.println("\n\n Task is cancelled --> " + Thread.currentThread());
 			}
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		} catch (ExecutionException e) {
-			e.printStackTrace();
-		} catch (TimeoutException e) {
-			bookFuture.cancel(true);
-			System.out.println("\n\nBook Task is cancelled --> " + Thread.currentThread());
 		}
 		
-		try {
-			Track track = trackFuture.get(TIME_FRAME, TimeUnit.NANOSECONDS);
-			List<Result> results = track.getResults();
-			for(Result result: results) {
-				Media m2 = new Media(result.getTrackName(),result.getArtistName(),MyMediaType.SONG);
-				if(!StringUtils.isEmpty(result.getTrackName()) && !StringUtils.isEmpty(result.getArtistName()) && !medias.equals(m2))
-					medias.add(m2);	
-			}
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		} catch (ExecutionException e) {
-			e.printStackTrace();
-		} catch (TimeoutException e) {
-			trackFuture.cancel(true);
-			System.out.println("\n\nTrack Task is cancelled --> " + Thread.currentThread());
-		}
-
 		executor.shutdown();
 		
-		Collections.sort(medias,new Comparator<Media>(){
-      	  public int compare(Media p1, Media p2){
-  		    return p1.getTitle().compareTo(p2.getTitle());
-  		  }
-  		}); 
-		
-	 return medias;
-
-}
+		return medias.parallelStream().sorted((m1,m2) -> m1.getTitle().compareTo(m2.getTitle())).collect(Collectors.toList());
+	}
 }
